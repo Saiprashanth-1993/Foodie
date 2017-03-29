e56f8cdd7523f170fbd3ebb7cf37f79d471c6053
Saiprashanth-1993 <saiprashanth2093@gmail.com>
2017-03-29 19:11:51 +0530

    animation added to like button

--- app/src/main/java/profoodies/com/profoodies/home/like/view/LikeFragment.java
+++ app/src/main/java/profoodies/com/profoodies/home/like/view/LikeFragment.java
@@ -12,6 +12,7 @@ import profoodies.com.profoodies.R;
 import profoodies.com.profoodies.databinding.FragmentLikeBinding;
 import profoodies.com.profoodies.home.like.model.LikeModel;
 import profoodies.com.profoodies.home.viewmodel.HomePageController;
+import xyz.hanks.library.SmallBang;
 
 /**
  * Created by Contus team on 24/3/17.
@@ -20,18 +21,24 @@ import profoodies.com.profoodies.home.viewmodel.HomePageController;
 public class LikeFragment extends Fragment {
 
     FragmentLikeBinding fragmentLikeBinding;
+    SmallBang smallBang;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         fragmentLikeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_like, container, false);
+
+        smallBang = SmallBang.attach2Window(getActivity());
+
         return fragmentLikeBinding.getRoot();
+
+
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        fragmentLikeBinding.setViewController(new HomePageController());
+        fragmentLikeBinding.setViewController(new HomePageController(smallBang));
         fragmentLikeBinding.setLikeInfo(new LikeModel());
     }
 }
--- app/src/main/java/profoodies/com/profoodies/home/viewmodel/HomePageController.java
+++ app/src/main/java/profoodies/com/profoodies/home/viewmodel/HomePageController.java
@@ -4,17 +4,28 @@ import android.view.View;
 
 import profoodies.com.profoodies.home.follow.model.FollowModel;
 import profoodies.com.profoodies.home.like.model.LikeModel;
+import xyz.hanks.library.SmallBang;
 
 /**
  * Created by Contus team on 29/3/17.
  */
 
 public class HomePageController {
+    SmallBang smallBang;
+
+    public HomePageController(){
+    }
+
+    public HomePageController(SmallBang smallBang){
+        this.smallBang = smallBang;
+    }
 
     public View.OnClickListener FavbtnClick(final LikeModel likeModel) {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                smallBang.bang(view);
                 likeModel.setToEditOrSave(true);
             }
         };
